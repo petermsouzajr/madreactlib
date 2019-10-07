e379c00ec553919c48f3a9f4c5dc651ae8759950
Peter M Souza Jr <petermsouzajr.@peters-mbp.home>
2019-10-06 19:26:33 -0700

    changed final talk, changing blur next

--- src/components/content.js
+++ src/components/content.js
@@ -9,14 +9,22 @@ class Content extends Component {
         this.child_count = 1;
     }
 
-    componentWillReceiveProps() {
+    UNSAFE_componentWillReceiveProps() {
         this.count = 1;
     }
 
     renderLabel(data){
         return [
-            <span className="card__content__label" key={this.child_count++}>{this.count++}</span>,
-            <b key={this.child_count++}>{data}</b>
+            
+            <span 
+                className="card__content__label" 
+                key={this.child_count++}>
+                    {this.count++}
+            </span>,
+            <b 
+                key={this.child_count++}>
+                    {data}
+            </b>
         ]
     }
 
@@ -29,7 +37,15 @@ class Content extends Component {
                     <img src={Michael} alt="Michael Scott" />
                 </div>
                 <p>
-                    Ladies and gentlemen, this is {this.renderLabel(data.color)} Barber, your sportscaster, bringing you the last inning of the game between the Cleveland <b>{this.renderLabel(data.pluralNoun)}</b> and the <b>{this.renderLabel(data.adjectiveOne)}</b> Yankees. <b>{this.renderLabel(data.celebOne)}</b> is pitching for the Yankees. Here's the pitch! It's a low <b>{this.renderLabel(data.adjectiveTwo)}</b> ball that just cuts the inside of the <b>{this.renderLabel(data.nounOne)}</b> for a strike. That makes the count <b>{this.renderLabel(data.numberOne)}</b> strikes and <b>{this.renderLabel(data.numberTwo)}</b> balls. Now here's the next pitch. The batter swings and connects. It's a long, high <b>{this.renderLabel(data.nounTwo)}</b> out to <b>{this.renderLabel(data.adjectiveThree)}</b> field. But <b>{this.renderLabel(data.celebTwo)}</b> is coming up fast and has it for the second out. The next batter up is <b>{this.renderLabel(data.celebThree)}</b>, the Cleveland <b>{this.renderLabel(data.adjectiveFour)}</b>-stop. Here's the pitch... and it's hit... a short ground ball to third <b>{this.renderLabel(data.nounThree)}</b>. <b>{this.renderLabel(data.celebFour)}</b> scoops it up and throws it to first base for an out, and the game is over. And the Yankees move into second place in the <b>{this.renderLabel(data.adjectiveFive)}</b> League!
+                    Well, <b>{this.renderLabel(data.color)}</b> <b>{this.renderLabel(data.pluralNoun)}</b>,  
+                    can I call you <b>{this.renderLabel(data.adjectiveOne)}</b>? Great! I call all my friends <b>{data.adjectiveOne}</b>.
+                    It's nice to finally <b>{this.renderLabel(data.celebOne)}</b> you, person-o a person-o -
+                    inclusivityness is our number <b>{this.renderLabel(data.adjectiveTwo)}</b> concern here. 
+                    It says here you've been at <b>{this.renderLabel(data.nounOne)}</b> corp. for 3 years, can I ask why youre looking for a <b>{this.renderLabel(data.numberOne)}</b>? 
+                    You know, <b>{this.renderLabel(data.numberTwo)}</b> like us are a rare breed, real road <b>{this.renderLabel(data.nounTwo)}</b>, always grasping at the <b>{this.renderLabel(data.adjectiveThree)}</b>. 
+                    Well, enough of the <b>{this.renderLabel(data.celebTwo)}</b> talk, lets get down to the <b>{this.renderLabel(data.celebThree)}</b> and <b>{this.renderLabel(data.adjectiveFour)}</b> of it, shall we? 
+                    Under strengths, you've listed: <b>{this.renderLabel(data.nounThree)}</b> and <b>{this.renderLabel(data.nounThree)}</b>, that's bold of you, I admire a good <b>{data.celebFour}</b>, 
+                    well, I think i've heard enough, <b>{this.renderLabel(data.adjectiveFive)}</b>!
                 </p>
             </div>
         )
